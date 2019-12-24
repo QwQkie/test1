9b589c6007dc767622fdbd747f0dea086044e79f
xiaoqianlong <1582529844@qq.com>
2019-12-24 14:43:04 +0800

    自己做的

---
+++ src/main/java/com/me/test1/Test1Application.java
@@ -0,0 +1,13 @@
+package com.me.test1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test1Application.class, args);
+    }
+
+}
